e79911bd7e7038868e69a680f156eed77192ecf6
Andrey Volchenskiy <andvolch@gmail.com>
2021-10-20 14:48:41 +0300

    copy files

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+import ContactForm from './components/ContactForm/ContactForm';
+import Filter from './components/Filter/Filter'; 
+import ContactList from './components/ContactList/ContactList';
+import dataBaseContacts from './data/contacts.json';
+
+class App extends Component {
+  state = {
+    contacts: dataBaseContacts,
+    filter: '',
+  
+  } 
+
+  deleteContact = contactId => {
+    this.setState(prewState => ({
+      contacts: prewState.contacts.filter(contact => contact.id !== contactId)
+    }))
+  }
+
+  
+  formSubmitHandler = contactic=> {
+    // console.log(contact);
+   
+    const check = this.state.contacts.some(contact => contact.name.toLowerCase() === contactic.name.toLowerCase());
+    
+      check ? alert(`${contactic.name} is already in contacts`)
+      : this.setState(({contacts}) => ({
+          contacts: [contactic, ...contacts],
+        }));
+
+  }
+
+  changeFilter = e => {
+    this.setState({filter: e.currentTarget.value})
+  }
+
+  getDisplayContacts = () => {
+    const {contacts, filter} = this.state;
+    const normalFilter = filter.toLowerCase();
+    
+    return contacts.filter(
+      contact => contact.name.toLowerCase().includes(normalFilter)
+    );
+  }
+
+  render () {
+    const {filter} = this.state;
+    const displayContacts = this.getDisplayContacts();
+
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm submit={this.formSubmitHandler}/>
+
+        <h2>Contacts</h2>
+        <Filter filter={filter} change={this.changeFilter}/>
+        <ContactList contacts={displayContacts} onDeleteContact={this.deleteContact}/>
+        
+      </div>)
+  }
+
+}
+
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+
+import PropTypes from 'prop-types';
+import shortid from 'shortid';
+
+import s from './ContactForm.module.css';
+
+
+class ContactForm extends Component {
+    state = {
+        id: shortid(),
+        name: '',
+        number: '',
+    };
+
+    handleChange = e => {
+        const {name, value} = e.currentTarget;
+        this.setState({
+            [name]: value,
+        });
+        // this.setState({name: e.currentTarget.value})
+    }
+
+    handleSubmint = e => {
+        e.preventDefault();
+        this.props.submit(this.state);
+        this.resetForm();
+        
+
+    }
+
+    resetForm = () => {
+        this.setState({
+            name: '',
+            number: '',
+        })
+    }
+        
+    render () {
+       const {name, number} = this.state;
+        return (
+            <div className={s.container}>
+                <form onSubmit={this.handleSubmint} className={s.form}>
+                    <h3>Name</h3>
+                        <label>
+                            <input
+                                type="text"
+                                name="name"
+                                value={name}
+                                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                                title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+                                required
+                                onChange={this.handleChange}
+                            />
+                        </label>
+                    <h3>Number</h3>
+                        <label>
+                            <input
+                                type="tel"
+                                name="number"
+                                value={number}
+                                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                                title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+                                required
+                                onChange={this.handleChange}
+                            />
+                        </label>
+                        <button type="submit" className={s.button}>Add contact</button>
+                </form>
+                
+                
+            </div>
+        )
+    };
+    
+    
+}
+ContactForm.propTypes = {
+    id: PropTypes.string,
+    name: PropTypes.string,
+    number: PropTypes.string,
+    submit: PropTypes.func.isRequired,
+    
+};
+
+export default ContactForm;
